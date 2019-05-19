fd3ca20bd8d7e09667467610ba9f4dcbf69989cf
Richard Gyiko <richard.gyiko@whizystems.com>
2019-05-19 11:19:06 +0200

    Initial commit.

---
+++ src/JsonSamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
